64b1a0491ac81e6cefd41cd1ddd84935eb6b0262
わたあめえ <Wataamee777@gmail.com>
2026-02-10 18:47:57 +0900

    Add manifest package for jp.me.wataame

---
+++ app/src/main/java/jp/me/wataame/HeadsetUtils.kt
@@ -0,0 +1,23 @@
+package jp.me.wataame
+
+import android.content.Context
+import android.media.AudioDeviceInfo
+import android.media.AudioManager
+
+object HeadsetUtils {
+    fun isHeadsetConnected(context: Context): Boolean {
+        val audioManager = context.getSystemService(AudioManager::class.java)
+        val devices = audioManager.getDevices(AudioManager.GET_DEVICES_OUTPUTS)
+        return devices.any { device ->
+            when (device.type) {
+                AudioDeviceInfo.TYPE_WIRED_HEADSET,
+                AudioDeviceInfo.TYPE_WIRED_HEADPHONES,
+                AudioDeviceInfo.TYPE_USB_HEADSET,
+                AudioDeviceInfo.TYPE_USB_DEVICE,
+                AudioDeviceInfo.TYPE_BLUETOOTH_A2DP,
+                AudioDeviceInfo.TYPE_BLUETOOTH_SCO -> true
+                else -> false
+            }
+        }
+    }
+}
